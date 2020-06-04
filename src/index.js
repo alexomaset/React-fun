a51765de799d1311292e989977ca74f53a06b26b
Alex omaset <alexomaset711@gmail.com>
2020-06-04 15:42:32 +0300

    nesting book component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+const Book = ({title, author, pages}) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p>Pages: {pages}</p>
+    </section>
+  )
+}
 const Library = () => {
   return (
     <div>
-      Welcome to the library
+     <Book title="River Between" author="Ngugi" pages="300" />
+     <Book title="River and The Source" author="Mary Ogola" pages="400" />
+     <Book title="Born a Crime" author="Trevor Noah" pages="500" />
     </div>
   )
 }
 
 
 render(
-  <div>
-    <Library />
-    <Library />
-    <Library />
-    <Library />
-  </div>
-,
+   <Library />,
   document.getElementById('root')
 )
