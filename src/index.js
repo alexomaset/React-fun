911fed107db11b789caa07105370e0245bbb2cca
Alex omaset <alexomaset711@gmail.com>
2020-06-04 15:12:04 +0300

    functional component

--- src/index.js
+++ src/index.js
@@ -7,40 +7,42 @@ let exersiceData = {
   goal: 10,
   backcountry: 10
 }
-class SkiDayCounter extends Component {
-  getPercent = decimal=> {
-    return decimal * 100 + '%'
-  }
 
-  getTotal = (total, goal) => {
-    return this.getPercent(total/goal)
-  }
+const getPercent = decimal=> {
+      return decimal * 100 + '%'
+    }
+  
+const getTotal = (total, goal) => {
+      return this.getPercent(total/goal)
+    }
 
-  render() {
-    const {total, powder, goal, backcountry} = this.props
-    return ( 
-     <section>
-       <div> 
-         <p>Total Days: {total}</p>
-         </div>
-         <div> 
-         <p>Total Powder: {powder}</p>
-         </div>
-         <div> 
-         <p> goal progress: {this.getTotal(total, goal)}</p>
-         </div>
-         <div> 
-         <p>Total backcountry: {backcountry}</p>
-         </div>
-     </section>
-    )
-  }
+const SkiDayCounter =({total, powder, goal, backcountry}) => {
+  return (
+    <section>
+    <div> 
+      <p>Total Days: {total}</p>
+      </div>
+      <div> 
+      <p>Total Powder: {powder}</p>
+      </div>
+      <div> 
+      <p> goal progress: {getPercent(total, goal)}</p>
+      </div>
+      <div> 
+      <p>Total backcountry: {backcountry}</p>
+      </div>
+  </section>
+  )
+
 }
+ 
+
 
 ReactDOM.render(
   <SkiDayCounter
   total={exersiceData.total}
   powder={exersiceData.powder}
-  goal={exersiceData.goal} />,
+  goal={exersiceData.goal}
+  backcountry={exersiceData.backcountry} />,
   document.getElementById('root')
 )
