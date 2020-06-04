60b54e4951d35cf3f9a18c27be57dee3edd6920f
Alex omaset <alexomaset711@gmail.com>
2020-06-04 15:31:45 +0300

    Component destructuring

--- src/index.js
+++ src/index.js
@@ -1,48 +1,22 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-let exersiceData = {
-  total: 50,
-  powder: 20,
-  goal: 10,
-  backcountry: 10
-}
-
-const getPercent = decimal=> {
-      return decimal * 100 + '%'
-    }
-  
-const getTotal = (total, goal) => {
-      return this.getPercent(total/goal)
-    }
-
-const SkiDayCounter =({total, powder, goal, backcountry}) => {
+const Library = () => {
   return (
-    <section>
-    <div> 
-      <p>Total Days: {total}</p>
-      </div>
-      <div> 
-      <p>Total Powder: {powder}</p>
-      </div>
-      <div> 
-      <p> goal progress: {getPercent(total, goal)}</p>
-      </div>
-      <div> 
-      <p>Total backcountry: {backcountry}</p>
-      </div>
-  </section>
+    <div>
+      Welcome to the library
+    </div>
   )
-
 }
- 
 
 
-ReactDOM.render(
-  <SkiDayCounter
-  total={exersiceData.total}
-  powder={exersiceData.powder}
-  goal={exersiceData.goal}
-  backcountry={exersiceData.backcountry} />,
+render(
+  <div>
+    <Library />
+    <Library />
+    <Library />
+    <Library />
+  </div>
+,
   document.getElementById('root')
 )
